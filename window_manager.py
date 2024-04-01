10fceeb2ff942cb5c237b1420d5f51ddcc5dfa5c
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-04-01 16:26:36 +0200

    Upgraded Window Manager

--- window_manager.py
+++ window_manager.py
@@ -1,18 +1,26 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget
 
+from image_window import ImageWindow
+
 
 class WindowManager:
     def __init__(self):
         self.windows = []
+        self.image_windows = []
 
     def add_window(self, window: QMainWindow | QWidget):
         self.windows.append(window)
+        if isinstance(window, ImageWindow):
+            self.image_windows.append(window)
 
     def remove_window(self, window: QMainWindow | QWidget) -> None:
         self.windows.remove(window)
+        if isinstance(window, ImageWindow):
+            self.image_windows.remove(window)
 
     def remove_all(self):
         self.windows.clear()
+        self.image_windows.clear()
 
 
 WINDOW_MANAGER = WindowManager()
