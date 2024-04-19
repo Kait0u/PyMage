9a7321b174f129b66cedb39a4e3accf711ec5139
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-04-19 15:51:41 +0200

    Added Bresenham's Line utility function.

--- main.py
+++ main.py
@@ -3,22 +3,42 @@ from PyQt5.QtWidgets import QApplication
 from main_window import MainWindow
 from forms.range_stretch_form import RangeStretchForm
 
+TEST = True
 
 def test():
+    import numpy as np
+    from utils import bresenham
+    from random import randint
+    import matplotlib.pyplot as plt
+
+
     p = r"C:\Users\jjaw-\Downloads\harold.jpg"
     # p = r"C:\Users\jjaw-\Downloads\lena.png"
 
-    app = QApplication(sys.argv)
-    from image import Image
-    img = Image.from_file(p, True)
-    img.posterize(2)
-    img = img.bitwise_not()
-    import numpy as np
-    from image import Padding
-    img.skeletonize(np.ones((3, 3)), Padding.REFLECT)
-    img.show()
-    # print(rsf.p1)
-    app.exec()
+    # app = QApplication(sys.argv)
+    # from image import Image
+    # img = Image.from_file(p, True)
+    # img.posterize(2)
+    # img = img.bitwise_not()
+
+    # from image import Padding
+    # img.skeletonize(np.ones((3, 3)), Padding.REFLECT)
+    # img.show()
+    # # print(rsf.p1)
+    # app.exec()
+
+    image = np.zeros((64, 64))
+    x1, y1, x2, y2 = randint(0, 63), randint(0, 63), randint(0, 63), randint(0, 63)
+    x1, y1, x2, y2 = 0, 0, 63, 15
+    print(x1, y1)
+    print(x2, y2)
+    line = bresenham(x1, y1, x2, y2)
+    print(line)
+    line = list(line)
+    for p in line:
+        image[p[0], p[1]] = 1
+    plt.imshow(image)
+    plt.show()
 
 
 def main():
@@ -29,5 +49,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-    # test()
+    if not TEST:
+        main()
+    else:
+        test()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 import numpy as np
 from scipy.signal import convolve2d
 
@@ -17,3 +19,59 @@ def cumsum(arr: np.array) -> np.ndarray:
 def convolve_filters(f1: np.ndarray, f2: np.ndarray) -> np.ndarray:
     return convolve2d(f1, f2, mode="full")
 
+
+def _bresenham_low(x1: int, y1: int, x2: int, y2: int) -> np.ndarray:
+    result = []
+    dx = x2 - x1
+    dy = y2 - y1
+    yi = 1
+    if dy < 0:
+        yi = -1
+        dy = -dy
+    d = 2 * dy - dx
+    y = y1
+
+    for x in range(x1, x2 + 1):
+        result.append((x, y))
+        if d > 0:
+            y += yi
+            d += 2 * (dy - dx)
+        else:
+            d += 2 * dy
+
+    return np.array(result)
+
+
+def _bresenham_high(x1: int, y1: int, x2: int, y2: int) -> np.ndarray:
+    result = []
+    dx = x2 - x1
+    dy = y2 - y1
+    xi = 1
+    if dx < 0:
+        xi = -1
+        dx = -dx
+    d = 2 * dx - dy
+    x = x1
+
+    for y in range(y1, y2 + 1):
+        result.append((x, y))
+        if d > 0:
+            x += xi
+            d += 2 * (dx - dy)
+        else:
+            d += 2 * dx
+
+    return np.array(result)
+
+
+def bresenham(x1: int, y1: int, x2: int, y2: int) -> np.ndarray:
+    if abs(y2 - y1) < abs(x2 - x1):
+        if x1 > x2:
+            return _bresenham_low(x2, y2, x1, y1)
+        else:
+            return _bresenham_low(x1, y1, x2, y2)
+    else:
+        if y1 > y2:
+            return _bresenham_high(x2, y2, x1, y1)
+        else:
+            return _bresenham_high(x1, y1, x2, y2)
