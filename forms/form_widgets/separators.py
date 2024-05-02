af6cd46b4993f7bf981923a2c7dbb7a419c6edce
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-05-02 19:54:40 +0200

    Added Horizontal and Vertical Separators

---
+++ forms/form_widgets/separators.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QFrame
+
+
+class HorizontalSeparator(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setFrameShape(QFrame.HLine)
+        self.setFrameShadow(QFrame.Sunken)
+
+
+class VerticalSeparator(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setFrameShape(QFrame.VLine)
+        self.setFrameShadow(QFrame.Sunken)
