5de41aaaf28f55345f60a9742e2916552d34d762
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-05-08 22:47:23 +0200

    Removed a silly unnecessary import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow
-from forms.range_stretch_form import RangeStretchForm
+from app.main_window import MainWindow
 
 
 def main():
