74d46aee7a82df67c660f6cce316ef78fe84ae94
Jakub Jaworski <jjaw.stuff@gmail.com>
2024-05-08 22:46:30 +0200

    Added RLE binarizer and parser

--- image_utils.py
+++ image_utils.py
@@ -22,3 +22,58 @@ def structuring_element(shape: StructuringElementShape, n: int):
 
     return result
 
+
+def make_binary_rle(rle: list[tuple[int, int]], height: int, width: int) -> bytearray:
+    header_structure = (4, 4)
+    entry_structure = (2, 4)
+
+    rle_bytes = bytearray()
+
+    header_vals = (height, width)
+    for idx, val in enumerate(header_vals):
+        v = int(val)
+        b = v.to_bytes(header_structure[idx], "big")
+        rle_bytes.extend(b)
+
+    for pair in rle:
+        for idx, val in enumerate(pair):
+            v = int(val)
+            b = v.to_bytes(entry_structure[idx], "big")
+            rle_bytes.extend(b)
+
+    return rle_bytes
+
+
+def parse_binary_rle(input_bytes: bytearray) -> np.ndarray:
+    header_structure = (4, 4)
+    entry_structure = (2, 4)
+
+    temp_bytes = input_bytes.copy()
+
+    height = int.from_bytes(temp_bytes[:header_structure[0]], byteorder="big")
+    del temp_bytes[:header_structure[0]]
+
+    width = int.from_bytes(temp_bytes[:entry_structure[1]], byteorder="big")
+    del temp_bytes[:entry_structure[1]]
+
+    result = np.zeros(height * width, dtype=np.uint8)
+    filled = 0
+
+    while temp_bytes:
+        entry_bytes = []
+        for length in entry_structure:
+            b = temp_bytes[:length]
+            entry_bytes.append(b)
+            del temp_bytes[:length]
+
+        # There are two items in that list
+
+        pixel_value_b, pixel_count_b = entry_bytes
+        pixel_value = int.from_bytes(pixel_value_b, byteorder="big")
+        pixel_count = int.from_bytes(pixel_count_b, byteorder="big")
+
+        result[filled: filled + pixel_count] = pixel_value
+        filled += pixel_count
+
+    result = result.reshape((height, width))
+    return result
